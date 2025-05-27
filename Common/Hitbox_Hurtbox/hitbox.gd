2a2a45026ab24057523083673e67bfaa0591221c
Legand <70213999+LegandGame@users.noreply.github.com>
2025-05-27 15:04:52 -0400

    added trash zone
    added basic trash zone scene and script that drains the player's health while they are inside it

--- Common/Hitbox_Hurtbox/hitbox.gd
+++ Common/Hitbox_Hurtbox/hitbox.gd
@@ -1,6 +1,6 @@
 class_name HitboxComponent extends Area3D
 
-@export var value : int = -1	# use negative values to deal damage. positive to heal
+@export var value : float = -1.0	# use negative values to deal damage. positive to heal
 
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)

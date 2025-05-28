d91473c7d424b8b8de7e342beaee45e02fea22f8
Legand <70213999+LegandGame@users.noreply.github.com>
2025-05-28 11:33:44 -0400

    capped framerate and recapturable mouse

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -65,6 +65,8 @@ func _physics_process(delta: float) -> void:
 
 # Capture & De-capture mouse
 func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("left_click"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	if event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 

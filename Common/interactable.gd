4d313b488092c7370508506576dd3e907eac59fc
Legand <70213999+LegandGame@users.noreply.github.com>
2025-06-07 09:00:54 -0400

    fixed seed collection bug

--- Common/interactable.gd
+++ Common/interactable.gd
@@ -9,11 +9,11 @@ func _ready() -> void:
 	body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body : Node3D) -> void:
-	if body.is_in_group("player"):
+	if body is Player:
 		interact_online.emit(body)
 		canInteract = true
 
 func _on_body_exited(body : Node3D) -> void:
-	if body.is_in_group("player"):
+	if body is Player:
 		interact_offline.emit(body)
 		canInteract = false
